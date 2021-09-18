e8f8f363a403e302922984cecafa21b960baaca8
S.K.Roshan <63838060+dev-Roshan-lab@users.noreply.github.com>
2021-09-18 20:51:13 +0530

    Update decision_tree.dart

--- decision tree/decision_tree.dart
+++ decision tree/decision_tree.dart
@@ -1,36 +1,46 @@
-gini_index(var groups, var classes) {
-  num n = 0;
+double gini_index(var groups, var classes) {
+  num n = 0.0;
   var content_count = [];
-  var gini = 0.0;
+
   //find n
-  for (int i = 0; i < groups.length; i++) {
+  for (var i = 0; i < groups.length; i++) {
     content_count.add((groups[i]).length);
-    print(content_count);
-    var size = (groups[i]).length;
   }
   content_count.forEach((var e) {
     n += e;
   });
+
   //calculate gini
+  var gini = 0.0;
   var size = 0.0;
-  getLastElement(var list) {
-    List output = [];
+  List getLastElement(var list) {
+    var output = [];
     list.forEach((List e) {
-      output.add(e[e.length]);
+      output.add(e[e.length - 1]);
     });
     return output;
   }
 
-  countAndCalc(var data, var to_count) {}
+  double countAndCalc(var data, var to_count) {
+    var count = 0.0;
+    data.forEach((e) {
+      if (e == to_count) {
+        count++;
+      }
+    });
+    return count;
+  }
 
-  groups.forEach((e) {
-    size = e.length;
-    if (size != 0) {
-      var score = 0;
+  for (int y = 0; y < groups.length; y++) {
+    size = groups[y].length.toDouble();
+    if (size != 0.0) {
+      var score = 0.0;
       classes.forEach((e) {
-        var p = getLastElement(groups)//count the number of e in getLastElement(groups)/size;
+        var p = countAndCalc(getLastElement(groups[y]), e) / size;
+        score += (p * p);
       });
+      gini += (1.0 - score) * (size / n);
     }
-  });
+  }
   return gini;
 }
